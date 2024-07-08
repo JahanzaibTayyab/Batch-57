417cd594a19b1f2cf1ff15fd707059fe39841cc2
Wajih Humayun Hashmi <wajihh57@gmail.com>
2024-07-08 15:35:22 +0500

    Error TS1378 Correction

--- learn-typescript/12-May/project00_calculator/operations.js
+++ learn-typescript/12-May/project00_calculator/operations.js
@@ -1,17 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.division = exports.multiply = exports.subtract = exports.sum = void 0;
 function sum(num1, num2) {
-    const result = num1 + num2;
-    console.log(`Sum  of ${num1} and ${num2} = ${result}`);
+    var result = num1 + num2;
+    console.log("Sum  of ".concat(num1, " and ").concat(num2, " = ").concat(result));
 }
+exports.sum = sum;
 function subtract(num1, num2) {
-    const result = num1 - num2;
-    console.log(`Subtract  of ${num1} and ${num2} = ${result}`);
+    var result = num1 - num2;
+    console.log("Subtract  of ".concat(num1, " and ").concat(num2, " = ").concat(result));
 }
+exports.subtract = subtract;
 function multiply(num1, num2) {
-    const result = num1 * num2;
-    console.log(`Multiply  of ${num1} and ${num2} = ${result}`);
+    var result = num1 * num2;
+    console.log("Multiply  of ".concat(num1, " and ").concat(num2, " = ").concat(result));
 }
+exports.multiply = multiply;
 function division(num1, num2) {
-    const result = num1 / num2;
-    console.log(`Division  of ${num1} and ${num2} = ${result}`);
+    var result = num1 / num2;
+    console.log("Division  of ".concat(num1, " and ").concat(num2, " = ").concat(result));
 }
-export { sum, subtract, multiply, division };
+exports.division = division;
